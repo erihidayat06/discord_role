4e25858ab5760ed2270d87b6360f922436bb3e8c
erihidayat06 <erihidayat549@gmail.com>
2025-02-18 14:18:24 +0700

    Rubah Responsif table

--- resources/views/admin/guild/index.blade.php
+++ resources/views/admin/guild/index.blade.php
@@ -17,7 +17,7 @@
         <div class="row">
             <div class="col-lg-12">
 
-                <div class="card">
+                <div class="card table-responsive">
                     <div class="card-body">
                         <h5 class="card-title">Datatables Guild</h5>
 
