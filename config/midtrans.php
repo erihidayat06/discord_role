c1cf20e6edf5ec632da7cf7ebb63dec315c4e252
erihidayat06 <erihidayat549@gmail.com>
2025-03-13 13:04:50 +0700

    Merubah url

--- config/midtrans.php
+++ config/midtrans.php
@@ -5,5 +5,5 @@ return [
     'client_key' => env('MIDTRANS_CLIENT_KEY'),
     'server_key' => env('MIDTRANS_SERVER_KEY'),
     'merchant_id' => env('MIDTRANS_MERCHANT_ID'),
-    'snap_url' => env('MIDTRANS_IS_PRODUCTION') ? 'https://app.midtrans.com/snap/snap.js' : 'https://app.sandbox.midtrans.com/snap/snap.js',
+    'snap_url' => env('MIDTRANS_URL'),
 ];
