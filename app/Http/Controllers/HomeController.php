1cde4dc47f78851112e4fa8e971f52db875d2a8a
erihidayat06 <erihidayat549@gmail.com>
2025-03-13 13:22:53 +0700

    Bisa

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,13 +12,6 @@ class HomeController extends Controller
     public function index()
     {
 
-        dd([
-            'is_production' => env('MIDTRANS_IS_PRODUCTION', false),
-            'client_key' => env('MIDTRANS_CLIENT_KEY'),
-            'server_key' => env('MIDTRANS_SERVER_KEY'),
-            'merchant_id' => env('MIDTRANS_MERCHANT_ID'),
-            'snap_url' => env('MIDTRANS_URL'),
-        ]);
         $period = Period::firstOrCreate([], [
             'periode' => now(),
         ]);
